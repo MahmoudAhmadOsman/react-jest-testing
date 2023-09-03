ea21461371d19e3eb90a8b35b59de398e4e55472
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-09-02 22:15:54 -0500

    added UserForm.test test file

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -3,23 +3,13 @@ import UserForm from "./UserForm";
 
 test("it shows two inputs and a button", () => {
 	//1. render the component
-
 	render(<UserForm />);
 
 	//2. Manipulate the rendered component OR find an element in it
-
 	const inputs = screen.getAllByRole("textbox");
 	const button = screen.getByRole("button");
 
 	//3. Assertion - Make tsure the component is doing what we expext it to do
-
 	expect(inputs).toHaveLength(2);
-  expect(button).toBeInTheDocument();
-  
-
-
-
-
-
-  
+	expect(button).toBeInTheDocument();
 });
