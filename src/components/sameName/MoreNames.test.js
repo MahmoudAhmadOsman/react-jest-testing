c3e11eadcbee91495b62c85f45a9ca4b3e9d016a
Mahmoud Osman <mahmoudosman.som@gmail.com>
2023-09-13 13:29:22 -0500

    fixed failing test issue on UserList.test.js file

--- src/components/sameName/MoreNames.test.js
+++ src/components/sameName/MoreNames.test.js
@@ -5,8 +5,12 @@ test("shows email and search input", () => {
 	render(<MoreNames />);
 
 	// Find the input elements by their ARIA roles
-	const emailInput = screen.getByRole("textbox", { name: /email/i });
-	const searchInput = screen.getByRole("textbox", { name: /search/i });
+	const emailInput = screen.getByRole("textbox", {
+		name: /email/i,
+	});
+	const searchInput = screen.getByRole("textbox", {
+		name: /search/i,
+	});
 
 	// Assert that the input elements are in the document
 	expect(emailInput).toBeInTheDocument();
